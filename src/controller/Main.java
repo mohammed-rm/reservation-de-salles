73f671cb300d632ca06d62ee21aabc239f0e13a0
mohammed_rm <mohammed.rmich@etu.univ-tours.fr>
2022-01-22 21:18:10 +0100

    update

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -14,9 +14,9 @@ public class Main {
 
     public static void main(String[] args){
 
+        HibernateUtils.init();
 
-        //HibernateUtils.init();
-        EventQueue.invokeLater(new Runnable() {
+        /*EventQueue.invokeLater(new Runnable() {
             public void run() {
                 try {
                     ApplicationWindow app = new ApplicationWindow();
@@ -24,11 +24,10 @@ public class Main {
                   e.printStackTrace();
               }
           }
-        });
+        });*/
 
-        /*
         User user = new User();
-        user.setIdUser(76);
+        user.setIdUser(7);
         user.setFirstName("bob");
         user.setLastName("tom");
         UserDAO us = new UserDAO();
@@ -38,23 +37,25 @@ public class Main {
         us.read();
 
         Room room = new Room();
-        room.setIdRoom(45);
+        room.setIdRoom(4);
         RoomDAO rm = new RoomDAO();
         rm.create(room);
         //rm.delete(room);
         rm.read();
 
         Reservation resa = new Reservation();
-        resa.setIdReservation(15);
+        resa.setIdReservation(1);
         resa.setUser(user);
         resa.setRoom(room);
         resa.setBeginTime(resa.stringToLDT("2022-01-21 10:00:00"));
         resa.setEndTime(resa.stringToLDT("2022-01-21 12:00:00"));
         ReservationDAO resD = new ReservationDAO();
         resD.create(resa);
-        //resD.delete(resa);*/
+        //resD.delete(resa);
+        //resD.update(resa);
+        resD.read();
 
-        //HibernateUtils.close();
+        HibernateUtils.close();
 
     }
 }
